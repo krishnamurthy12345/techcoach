7550adab0f5b23bc1fdf10646c781dc0e6d6f1ed
krishnamurthy12345 <murthykichu@gmail.com>
2024-03-25 16:27:10 +0530

    localstorage  token

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Login.css';
 
 const Login = () => {
 
-  const loginwithgoogle = ()=>{
-    window.open(`${process.env.REACT_APP_API_URL}/auth/google`,'_self')
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+    if (token) {
+      localStorage.setItem('token', token);
+      window.location.href = '/dashboard';
+    }
+  }, []);
+
+  const loginwithgoogle = () => {
+    window.open(`${process.env.REACT_APP_API_URL}/auth/google`, '_self');
   }
+
   return (
     <div className='login-page'>
-      <h1 style={{textAlign:'center'}}>Login</h1>
+      <h1 style={{ textAlign: 'center' }}>Login</h1>
       <div className='form'>
         <form className='login-form'>
           <input type='text' name='' id='#name' placeholder='username' />
           <input type='password' name='' id='#pswd' placeholder='password' />
           <button>Login</button>
-          <p className='message'>Not Registred? <a href='#'>Create an Account</a></p>
+          <p className='message'>Not Registered? <a href='#message'>Create an Account</a></p>
         </form>
-        <button className='login-with-google-btn' onClick={loginwithgoogle}>Signin  with google</button>
+        <button className='login-with-google-btn' onClick={loginwithgoogle}>Sign in with Google</button>
       </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
