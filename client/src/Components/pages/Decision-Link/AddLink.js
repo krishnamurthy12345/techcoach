2a594864d30a06e31955db505a32cbddad97592c
krishnamurthy12345 <murthykichu@gmail.com>
2024-08-22 12:21:20 +0530

    error

--- client/src/Components/pages/Decision-Link/AddLink.js
+++ client/src/Components/pages/Decision-Link/AddLink.js
@@ -200,8 +200,8 @@ import './AddLink.css';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { IoIosInformationCircle } from "react-icons/io";
 import withAuth from '../../withAuth';
+import { IoIosInformationCircle } from "react-icons/io";
 import { useNavigate, Link, useParams, useLocation } from 'react-router-dom';
 
 const AddLink = () => {
